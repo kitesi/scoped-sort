9c7238e9f885c1940d4e529be8bfdcdb9e31e2dd
karizma <karizma7@protonmail.com>
2022-08-10 17:15:27 -0400

    add error handling for sort arguments

--- cli/src/parse-string-arguments.ts
+++ cli/src/parse-string-arguments.ts
@@ -1,4 +1,4 @@
-import yargs from 'yargs';
+import yargs, { options } from 'yargs';
 
 import type { Argv } from 'yargs';
 import type { Options } from 'string-content-sort';
@@ -77,6 +77,16 @@ export function genericSortYargsBuilder(y: Argv) {
     });
 }
 
+type YargsBaseArgumentsReturn = ReturnType<
+    ReturnType<typeof genericSortYargsBuilder>['parseSync']
+>;
+
+export type YargsCliArgumentsReturn = YargsBaseArgumentsReturn & {
+    files?: string[];
+    modify?: boolean;
+    useSortComments?: boolean;
+};
+
 export function parseStringArguments(args: string) {
     const parsedArgs = genericSortYargsBuilder(yargs([]))
         .help(false)
@@ -86,87 +96,25 @@ export function parseStringArguments(args: string) {
         .strict(true)
         .parseSync(args);
 
-    // prob a better way to do this
+    return getCleanOptions(parsedArgs);
+}
+
+export function getCleanOptions(args: YargsBaseArgumentsReturn) {
     const options: Options = {
-        sortNaturally: parsedArgs.sortNaturally,
-        sortByFloat: parsedArgs.sortByFloat,
-        sortByLength: parsedArgs.sortByLength,
-        sortNumerically: parsedArgs.sortNumerically,
-        sortRandomly: parsedArgs.sortRandomly,
-        reverse: parsedArgs.reverse,
-        recursive: parsedArgs.recursive,
-        unique: parsedArgs.unique,
-        caseInsensitive: parsedArgs.caseInsensitive,
-        useMatchedRegex: parsedArgs.useMatchedRegex,
-        markdown: parsedArgs.markdown,
-        regexFilter: parseStringAsRegex(parsedArgs.regex),
-        sectionSeperator: parseStringAsRegex(parsedArgs.sectionSeperator),
+        sortNaturally: args.sortNaturally,
+        sortByFloat: args.sortByFloat,
+        sortByLength: args.sortByLength,
+        sortNumerically: args.sortNumerically,
+        sortRandomly: args.sortRandomly,
+        reverse: args.reverse,
+        recursive: args.recursive,
+        unique: args.unique,
+        caseInsensitive: args.caseInsensitive,
+        useMatchedRegex: args.useMatchedRegex,
+        markdown: args.markdown,
+        regexFilter: parseStringAsRegex(args.regex),
+        sectionSeperator: parseStringAsRegex(args.sectionSeperator),
     };
 
-    const argumentDescriptions = {
-        e: 'naturally',
-        f: 'by float',
-        l: 'by length',
-        n: 'numerically',
-        z: 'randomly',
-    };
-
-    type SortAlias = 'e' | 'f' | 'l' | 'n' | 'z';
-    const sorters: Set<SortAlias> = new Set();
-
-    for (const sortAlias of ['e', 'f', 'l', 'n', 'z']) {
-        if (parsedArgs[sortAlias]) {
-            sorters.add(sortAlias as SortAlias);
-        }
-    }
-
-    if (options.regexFilter && options.sortRandomly) {
-        throw new Error("You can't sort by random and use a regex pattern");
-    }
-
-    if (options.regexFilter && options.sortNaturally) {
-        throw new Error("You can't sort naturally and use a regex pattern");
-    }
-
-    if (sorters.size > 1) {
-        const [firstSorter, secondSorter] = sorters.values();
-
-        throw new Error(
-            `You can't use sort ${argumentDescriptions[firstSorter]} and sort ${argumentDescriptions[secondSorter]} together`
-        );
-    }
-
-    if (options.caseInsensitive && !options.unique) {
-        for (const sorter of sorters) {
-            throw new Error(
-                `You can't use sort case insensitively with sort ${argumentDescriptions[sorter]} without the unique argument`
-            );
-        }
-    }
-
-    /*
-        commented it out because there is a valid use case,
-        when users by default want to use the matched regex,
-        but of course they won't always be using regexs,
-        if this threw an error, they would have to manunally
-        type 'm' each time.
-    */
-    // if (options.useMatchedRegex && !options.regex) {
-    //     throw new Error("You can't use the m argument without a regex pattern");
-    // }
-
-    // need to do all this to remove the undefined properties, maybe a bit slow on runtime though
-    const regexFilter = options.regexFilter;
-    const sectionSeperator = options.sectionSeperator;
-    const cleanOptions: Options = JSON.parse(JSON.stringify(options));
-
-    if (regexFilter) {
-        cleanOptions.regexFilter = regexFilter;
-    }
-
-    if (sectionSeperator) {
-        cleanOptions.sectionSeperator = sectionSeperator;
-    }
-
-    return cleanOptions;
+    return options;
 }
